d0bd9d33d0dd5d8bb319f2a03434c5d4a3561a0c
Muhammad Muzamil <muhammad.muzamilnisar@gmail.com>
2025-05-18 18:52:00 +0500

    first commit

---
+++ assets/js/loader.js
@@ -0,0 +1,11 @@
+// // Function to hide the loader
+// function hideLoader() {
+//     const loader = document.getElementById('loader');
+//     loader.style.display = 'none' ;
+//   }
+  
+//   // Wait for the page to fully load before hiding the loader
+//   window.addEventListener('load', () => {
+//     // hideLoader();
+//   });
+  
